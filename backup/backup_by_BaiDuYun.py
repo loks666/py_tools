b25c9858bce3496c6877a4d03fcaac6f2bf56678
jax <284190056@qq.com>
2023-12-23 04:32:26 +0800

    developed complete

--- backup/backup_by_BaiDuYun.py
+++ backup/backup_by_BaiDuYun.py
@@ -3,23 +3,29 @@ import json
 import os
 import shutil
 import time
+import logging
 from datetime import datetime
 
 import requests
 from bypy import ByPy
 
 
+# 设置日志
+logging.basicConfig(filename='backup_by_BaiDuYun.log', level=logging.INFO)
+
+
 def backup_blog(prefix, port, user, password, backup_path):
     # 网站地址
     website = f"http://localhost:{port}"
     # halo2备份文件夹路径
-    backup_halo_path = "/root/.jiewen/backups"
-    backup_api = website + f"/apis/{prefix}.superxiang.com/v1alpha1/backups"
-    check_api = website + f"/apis/{prefix}.superxiang.com/v1alpha1/backups?sort=metadata.creationTimestamp%2Cdesc"
+    backup_halo_path = f"/data/blog/{prefix}/backups"
+
+    backup_api = website + f"/apis/migration.halo.run/v1alpha1/backups"
+    check_api = website + f"/apis/migration.halo.run/v1alpha1/backups?sort=metadata.creationTimestamp%2Cdesc"
 
     # 获取现在的时间 2023-09-24T13:14:18.650Z
     now_time = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
-    print(now_time)
+    logging.info(now_time)
     # 构建认证头部
     auth_header = "Basic " + base64.b64encode((user + ":" + password).encode()).decode()
     payload = json.dumps({
@@ -39,40 +45,49 @@ def backup_blog(prefix, port, user, password, backup_path):
         'Authorization': "Basic " + base64.b64encode((user + ":" + password).encode()).decode(),
     }
     response = requests.request("POST", backup_api, headers=headers, data=payload)
-    print(response.text)
+    logging.info(response.text)
     if response.status_code == 201:
-        print("备份请求成功！")
-        new_backup_name = ""
+        logging.info(f"{prefix}备份请求成功！")
         while True:
             check_response = requests.request("GET", check_api, headers=headers)
             if check_response.status_code == 200:
                 backup_data = json.loads(check_response.text)
                 items = backup_data.get("items", [])
                 if items[0]["status"]["phase"] == "SUCCEEDED":
-                    print("备份完成！")
+                    logging.info(f"{prefix}备份完成！")
                     new_backup_name = items[0]["status"]["filename"]
                     break
                 if items[0]["status"]["phase"] == "RUNNING":
-                    print("正在备份！")
+                    logging.info(f"{prefix}正在备份！")
                     time.sleep(10)
 
             else:
-                print(f"查询备份请求失败！错误代码：{check_response.status_code}")
+                logging.error(f"{prefix}查询备份请求失败！错误代码：{check_response.status_code}")
         shutil.copy(backup_halo_path + "/" + new_backup_name, backup_path + "/" + new_backup_name)
-        print("备份文件复制完成！")
+        logging.info(f"{prefix}备份文件复制完成！")
 
     else:
-        print(f"备份请求失败！错误代码：{response.status_code}")
+        logging.error(f"{prefix}备份请求失败！错误代码：{response.status_code}")
+
+
+def ignore_sock_files(dirname, filenames):
+    return [name for name in filenames if name.endswith('.sock')]
 
 
 def backup_mysql(backup_path):
     # 复制MySQL数据
-    shutil.copytree("/data/mysql", backup_path + "/mysql")
+    dst_path = backup_path + "/mysql"
+    if os.path.exists(dst_path):
+        shutil.rmtree(dst_path)
+    shutil.copytree("/data/mysql", dst_path, ignore=ignore_sock_files)
 
 
 def backup_redis(backup_path):
     # 复制Redis数据
-    shutil.copytree("/data/redis", backup_path + "/redis")
+    dst_path = backup_path + "/redis"
+    if os.path.exists(dst_path):
+        shutil.rmtree(dst_path)
+    shutil.copytree("/data/redis", dst_path, ignore=ignore_sock_files)
 
 
 def zip_and_upload(directory):
@@ -80,6 +95,9 @@ def zip_and_upload(directory):
     shutil.make_archive(directory, 'zip', directory)
     bp = ByPy()
     bp.upload(localpath=directory + ".zip", remotepath="/backup/" + directory + ".zip", ondup='overwrite')
+    # 删除原始文件夹
+    shutil.rmtree(directory)
+    logging.info(f"{directory} has been deleted after zipping and uploading.")
 
 
 if __name__ == '__main__':
@@ -88,8 +106,8 @@ if __name__ == '__main__':
     os.makedirs(date_folder, exist_ok=True)
 
     # 调用函数进行备份
-    blogs = [('博客名', '端口号', '用户名', '密码'), ('博客名', '端口号', '用户名', '密码'),
-             ('博客名', '端口号', '用户名', '密码'), ('博客名', '端口号', '用户名', '密码')]
+    blogs = [('博客', '端口号', '管理员用户名', '管理员密码'), ('博客', '端口号', '管理员用户名', '管理员密码'),
+             ('博客', '端口号', '管理员用户名', '管理员密码'), ('博客', '端口号', '管理员用户名', '管理员密码')]
     for prefix, port, user, password in blogs:
         os.makedirs(f"{date_folder}/halo/{prefix}", exist_ok=True)
         backup_blog(prefix, port, user, password, f"{date_folder}/halo/{prefix}")
